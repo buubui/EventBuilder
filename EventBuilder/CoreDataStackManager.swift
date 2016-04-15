699de0903386a6db4b2d7e3224ebe7f8add4a151
Buu Bui <buu.bui@eastagile.com>
2016-04-15 15:52:07 +0700

    [r] Refactor CoreDataStackManager

--- EventBuilder/CoreDataStackManager.swift
+++ EventBuilder/CoreDataStackManager.swift
@@ -83,15 +83,12 @@ class CoreDataStackManager {
   // MARK: - Core Data Saving support
 
   func saveContext () {
-    if masterContext.hasChanges {
+    if mainQueueContext.hasChanges {
       do {
-        try masterContext.save()
+        try mainQueueContext.save()
       } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
         let nserror = error as NSError
         NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-        abort()
       }
     }
   }
